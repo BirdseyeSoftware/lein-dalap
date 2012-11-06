eedfa571ade032f24bbc817b4e64f14c4bbff370
Roman Gonzalez <romanandreg@gmail.com>
2012-11-06 00:44:59 +0000

    Adding the cljsbuild-dalap.transform ns

---
+++ src/cljsbuild_dalap/transform.clj
@@ -0,0 +1,73 @@
+(ns cljsbuild-dalap.transform
+  (:require [clojure.string :as str]
+            [dalap.walk]
+            [dalap.selector]
+            [cljsbuild-dalap.transform-rules :refer
+             [cljs-default-transform-rules]])
+
+  (:import [clojure.lang LineNumberingPushbackReader]))
+
+(defn visit-clj-form [form w]
+  ;; a modified version of clojure.walk with the ability to drop forms
+  (letfn [(filter-map [f form] (remove #(= % ::drop) (map f form)))]
+    (cond
+      (list? form) (apply list (filter-map w form))
+      (instance? clojure.lang.IMapEntry form) (vec (filter-map w form))
+      (seq? form) (doall (filter-map w form))
+      (coll? form) (into (empty form) (filter-map w form))
+      :else form)))
+
+(defn clj-forms-to-cljs-forms
+  ([forms]
+     (clj-forms-to-cljs-forms forms cljs-default-transform-rules))
+  ([forms rules]
+     (dalap.walk/walk forms
+                      ((dalap.selector/-gen-decorator rules) visit-clj-form))))
+
+(defn read-forms-from-file
+  ;; stolen from:
+  ;; https://github.com/jonase/kibit/blob/master/src/kibit/check.clj
+  "Gen a lazy sequence of top level forms from a LineNumberingPushbackReader"
+  [^LineNumberingPushbackReader r]
+  (lazy-seq
+   (let [form
+         (try
+           (read r false ::eof)
+           (catch Exception e
+             (throw (Exception.
+                     (str "Dalap's reader crashed"
+                          (.getMessage e)) e))))]
+     (when-not (= form ::eof)
+       (cons form (read-forms-from-file r))))))
+
+(defn read-clj-forms-from-input [input]
+  (read-forms-from-file
+   (LineNumberingPushbackReader.
+    ;; IMPORTANT:
+    ;; this piece of code replaces _top level_ #_(:cljs)
+    ;; forms with a (do) so that forms inside it get executed
+    ;; only in clojurescript
+    ;; e.g:
+    ;; #_(:cljs (println "hello world")) => (do (println "hello world"))
+    (java.io.StringReader.
+     (str/replace (slurp input) #"\#_\(\s*:cljs\b" "(do ")))))
+
+(defn cljs-generated-file-notice [clj-file-path]
+  (str ";; This file was generated from \n;;\n;; " clj-file-path
+       " @ " (java.util.Date.)
+       "\n;;\n"))
+
+(defn transform-to-cljs-file
+  ([input]
+     (transform-to-cljs-file input cljs-default-transform-rules))
+  ([input transform-rules]
+     (let [;;input-file (as-file input)
+           ;;path (.getPath f)
+           ;; rules (if (or (fn? transform-rules)
+           ;;               (map? transform-rules))
+           ;;         (transform-rules path)
+           ;;         transform-rules)
+           cljs-forms (clj-forms-to-cljs-forms
+                         (read-clj-forms-from-input input) transform-rules)]
+       (str (cljs-generated-file-notice input)
+            (str/join "\n" cljs-forms)))))
