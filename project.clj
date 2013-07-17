d958d416dff94c877e011106bbf91d1d7513edba
Tavis Rudd <tavis@damnsimple.com>
2013-07-17 12:02:00 -0700

    v0.1.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.birdseye-sw/lein-dalap "0.1.1-SNAPSHOT"
+(defproject com.birdseye-sw/lein-dalap "0.1.1"
   :description "Provides clojure to clojurescript transformation using dalap"
   :url "http://birdseye-sw.com/oss/lein-dalap/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.birdseye-sw/dalap "0.1.0"]
-                 [fs "1.1.2"]
+                 [fs "1.3.3"]
                  [watchtower "0.1.1"]
-                 [lein-cljsbuild "0.3.0"]]
+                 [lein-cljsbuild "0.3.2"]]
   :eval-in-leiningen true)
